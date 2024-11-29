7419c1b015aa345fd8b5ad3c04d52977dbb64c1b
haylee360 <hayleeoyler360@gmail.com>
2024-11-28 17:07:15 -0800

    Work on final map

--- aqua_fun.R
+++ aqua_fun.R
@@ -1,10 +1,21 @@
 
-
-# minimum and maximum sea surface temperature
-# minimum and maximum depth
-# species name
-
-aqua_fun <- function(species, max_sst, min_sst, max_depth, min_depth){
+#' Find Aquatic Species Suitable Habitat
+#' 
+#' @description
+#' This function takes an aquatic species' sea surface temperature and depth requirements and visualizes where its suitable habitat is along the west coast of the United States.
+#'
+#' @param species str, the name of the species of interest
+#' @param min_sst float, the minimum sea surface temperature of the species
+#' @param max_sst float, the maximum sea surface temperature of the species
+#' @param min_depth int, the minimum depth of the species
+#' @param max_depth int, the maximum depth of the species
+#'
+#' @return A map of the suitable living areas of the species of interest
+#' @export
+#'
+#' @examples
+#' aqua_fun("Pacific Sea Star", 24.7, 29.3, -30, 0)
+aqua_fun <- function(species, min_sst, max_sst, min_depth, max_depth){
   
   # Read in EEZ and Depth data
   eez <- read_sf(here("data", "wc_regions_clean.shp"))
@@ -38,16 +49,16 @@ aqua_fun <- function(species, max_sst, min_sst, max_depth, min_depth){
                       max_sst, Inf, NA),
                     ncol = 3, byrow = TRUE)
   
-  # use reclassification matrix to reclassify sst raster
+  # Use reclassification matrix to reclassify sst raster
   reclass_sst <- classify(mean_sst, rcl = rcl_sst)
   
-  # create depth reclassification matrix
+  # Create depth reclassification matrix
   rcl_depth <- matrix(c(-Inf, min_depth, NA, 
                         min_depth, max_depth, 1, 
                         max_depth, Inf, NA),
                       ncol = 3, byrow = TRUE)
   
-  # use reclassification matrix to reclassify depth raster
+  # Use reclassification matrix to reclassify depth raster
   reclass_depth <- classify(depth_resample, rcl = rcl_depth)
   
   # Find locations that satisfy both sst and depth conditions
@@ -59,28 +70,36 @@ aqua_fun <- function(species, max_sst, min_sst, max_depth, min_depth){
   # Clip by eez raster
   sst_depth_eez <- sst_depth[eez_raster, drop = FALSE]
 
-  # Find area of grid cells in suitable locations
-  area_cell <- cellSize(sst_depth_eez, unit = "km")
+  # Find area of suitable cells 
+  suitable_area <- cellSize(x = sst_depth, # places where sst and depth match
+                            mask = TRUE, # keeps NA values
+                            unit = 'km') # Selecting km from data
   
-  eez_raster <- rasterize(west_eez,
+  eez_raster <- rasterize(eez,
                           area_cell,
                           field = 'rgn')
 
   
-  # Use zonal algebra to aggregate a grouping variable
-  eez_suitable <- zonal(x = area_cell, 
+  # Use zonal algebra to sum the suitable area by region
+  eez_suitable <- zonal(x = suitable_area, 
                         z = eez_raster, # Raster representing zones
                         fun = 'sum', # To add up total area
                         na.rm = TRUE)
   
+  # Plot regions and suitable areas together
+  tm_shape(depth) +
+    tm_raster(palette = "-GnBu",
+              # alpha = 0.5,
+              midpoint = 0,
+              legend.show = FALSE) +
+    tm_shape(eez_raster) +
+    tm_raster(palette = "Accent",
+              alpha = 0.7,
+              title = "Region") +
+    tm_shape(sst_depth) +
+    tm_raster(palette = "Set1",
+              title = "Suitable habitat") +
+    tm_layout(legend.outside = TRUE,
+              frame = FALSE,
+              main.title = print(paste0("West Coast Exclusive Economic Zones (EEZ) best suited to", species, "marine aquaculture")))
   }
-
-
-# process SST (mean, Cel)
-# crop, resample
-# reclassify
-# sst depth multiplication
-# rasterize eez
-# crop and mask to eez
-# find area of grid cells
-# find total suitable area
